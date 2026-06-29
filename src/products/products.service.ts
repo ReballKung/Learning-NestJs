3c57026ebc1612f58221eb3218c5f632839d0e59
Sukrit Saeliao <jamesballzaza1159@gmail.com>
2026-06-29 15:25:48 +0700

    feat: workshop 3 = PostgreSQL/Prisma

---
+++ src/products/products.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class ProductsService {
+  constructor(private prisma: PrismaService) { }
+
+  async create(createProductDto: CreateProductDto) {
+    return this.prisma.product.create({
+      data: createProductDto,
+    });
+  }
+
+  async findAll() {
+    return this.prisma.product.findMany();
+  }
+
+  async findOne(id: number) {
+    const product = await this.prisma.product.findUnique({ where: { id } });
+    if (!product) {
+      throw new NotFoundException(`ไม่พบสินค้าที่ ID เป็น ${id}`);
+    }
+    return product;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    return this.prisma.product.update({
+      where: { id },
+      data: updateProductDto,
+    });
+  }
+
+  async remove(id: number) {
+    return this.prisma.product.delete({
+      where: { id },
+    });
+  }
+}
